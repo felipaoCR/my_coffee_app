7c1579b6b7ef32b00a63575b6f6e6d372ee7e749
felipaoCR <moyafelipe94@gmail.com>
2014-07-17 10:13:17 -0600

    Arduino code
    I´ll improve this code later ...

---
+++ duino.ino
@@ -0,0 +1,23 @@
+int output1 = 13;  //These are the duinos outputs, which are
+int output2 = 6;  //considered as the relay´s inputs, feel free to pick whatever duino´s pin you like
+
+void setup()
+{
+  pinMode(output1, OUTPUT);
+  pinMode(output2, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  if(Serial.available()>0)       //If data is received, then send a +5 volts input to the power switch tail
+    {
+      digitalWrite(output1, HIGH);
+      digitalWrite(output2, HIGH);
+    }
+  else
+    {
+      digitalWrite(output1, LOW);  //If not, then do nothing
+      digitalWrite(output2, LOW);
+    }  
+}
