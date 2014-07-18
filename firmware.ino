fe5b2ef27ac679eaee596c6316dcfe2336f796a2
felipaoCR <moyafelipe94@gmail.com>
2014-07-18 09:12:26 -0600

    Update firmware.ino

--- firmware.ino
+++ firmware.ino
@@ -10,14 +10,20 @@ void setup()
 
 void loop()
 {
-  if(Serial.available()>0)       //If data is received, then send a +5 volts input to the power switch tail
-    {
+  if(Serial.available()>0)       //If data is available on input buffer, and data on buffer isn't NONE, send a 
+    {                           // HIGH +5 volts signal to power switch tail
+      if(Serial.read()!=-1)
+      {
+      
       digitalWrite(output1, HIGH);
       digitalWrite(output2, HIGH);
-    }
+      delay(3000);                 //This delay goes according to your write serial function delay in the sudo_make_coffee.py code
+                                  //I set a three seconds delay to give the write python function enough time to send data
+      }                           //and fill the incoming buffer in the arduino (Since my write delay lasts for a sec).
+    }                             //That way there will be always data in the input buffer, as long as your python code sends some data.
   else
     {
-      digitalWrite(output1, LOW);  //If not, then do nothing
-      digitalWrite(output2, LOW);
+      digitalWrite(output1, LOW);  //Because of the delay, it'll last at least around ten to fifteen seconds for the arduino to 
+      digitalWrite(output2, LOW); // stop sending HIGH signals to the relays in the power switch tail
     }  
 } 
